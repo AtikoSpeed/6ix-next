099e92d623f29fbea09b96547dd585bb2ba1d934
Atakan Temizkan <atikotemizkan@gmail.com>
2025-02-22 21:35:59 +0300

    modified:   src/app/layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,13 +1,16 @@
-import { Geist, Geist_Mono } from "next/font/google";
+import { Roboto, Roboto_Mono } from "next/font/google";
 import "@/styles/globals.css";
+import "@/styles/Layout.sass";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
+const robotoSans = Roboto({
+  variable: "--font-roboto-sans",
+  weight: ["400"],
   subsets: ["latin"],
 });
 
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const robotoMono = Roboto_Mono({
+  variable: "--font-roboto-mono",
+  weight: ["400"],
   subsets: ["latin"],
 });
 
@@ -20,7 +23,7 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+        className={`${robotoSans.variable} ${robotoMono.variable} antialiased`}
       >
         {children}
       </body>
