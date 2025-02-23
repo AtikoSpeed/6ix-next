7320e44fa158ec4e9d4764f95d70613062dc39ee
Atakan Temizkan <atikotemizkan@gmail.com>
2025-02-23 18:19:04 +0300

    rename to jsx

---
+++ src/app/layout.jsx
@@ -0,0 +1,22 @@
+import { Roboto } from "next/font/google";
+import "@/styles/globals.css";
+import "@/styles/Layout.sass";
+
+const robotoSans = Roboto({
+  variable: "--font-roboto-sans",
+  weight: ["400"],
+  subsets: ["latin"],
+});
+
+export const metadata = {
+  title: "6ixarchive",
+  description: "6ixarchive",
+};
+
+export default function RootLayout({ children }) {
+  return (
+    <html lang="en">
+      <body className={`${robotoSans.variable} antialiased`}>{children}</body>
+    </html>
+  );
+}
