9f4a1bade06caf8b03da512b1165d5da40a9230e
Atakan Temizkan <atikotemizkan@gmail.com>
2025-02-24 18:13:48 +0300

    add placeholders

--- src/app/[page]/page.jsx
+++ src/app/[page]/page.jsx
@@ -1,8 +1,24 @@
+import Card from "@/components/Card";
+
 export default async function CategoryPage({ params }) {
   const { page } = await params;
-  return <div>{page}</div>;
+  if (page === "menswear" || page === "womenswear") {
+    return (
+      <div className="container h-[calc(100vh-8rem)] max-w-screen grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] auto-rows-min gap-4 overflow-auto overflow-x-hidden p-2 sm:p-4">
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+        <Card />
+      </div>
+    );
+  }
 }
 
 // export function generateStaticParams() {
-// return [{ category: "menswear" }, { category: "womenswear" }];
+// return [{ page: "menswear" }, { page: "womenswear" }];
 // }
