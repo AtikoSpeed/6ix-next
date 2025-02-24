067d5bfbd92f537415eaec13a36928b4f0975ac5
Atakan Temizkan <atikotemizkan@gmail.com>
2025-02-24 15:30:21 +0300

    styling

--- src/app/[page]/layout.jsx
+++ src/app/[page]/layout.jsx
@@ -5,11 +5,11 @@ import ThemeToggle from "@/components/ThemeToggle";
 export default async function ShopLayout({ children, params }) {
   const parameters = await params;
   return (
-    <div className="antialiased overflow-hidden">
+    <div className="antialiased overflow-hidden h-screen">
       <Sidebar props={parameters}>
         <Navbar props={parameters} />
-        <main className="flex-1 w-full lg:w-[calc(100vw-13rem)] h-full grid grid-cols-12 grid-rows-12 auto-rows-max auto-cols-max p-2 overflow-y-auto">
-          <div className="w-fit h-fit col-start-12 row-start-1 justify-self-end m-2">
+        <main className="w-full lg:w-[calc(100vw-13rem)] h-[calc(100vh-4rem)] grid grid-cols-1 auto-rows-min p-2 overflow-hidden">
+          <div className="w-fit h-fit col-start-1 row-start-1 justify-self-end m-2">
             <ThemeToggle />
           </div>
           {children}
