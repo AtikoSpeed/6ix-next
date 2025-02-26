9469b66d84b10da109714308746cdc022baa78c0
Atakan Temizkan <atikotemizkan@gmail.com>
2025-02-26 14:29:55 +0300

    add fetch from strapi and map to cards

--- src/app/[page]/page.jsx
+++ src/app/[page]/page.jsx
@@ -1,24 +1,48 @@
 import Card from "@/components/Card";
+import Link from "next/link";
+
+async function getPageData(page) {
+  const res = await fetch(
+    `https://strapi.6ixarchive.com/api/items?filters[category][$eq]=${page}&populate=itemPic`,
+    {
+      method: "GET",
+      headers: {
+        // eslint-disable-next-line no-undef
+        Authorization: `bearer ${process.env.STRAPI_API_TOKEN}`,
+      },
+    },
+    { next: { revalidate: 60 } }
+  );
+
+  // if (!res.ok) {
+  // throw new Error("Failed to fetch data");
+  // }
+
+  return res.json();
+}
 
 export default async function CategoryPage({ params }) {
   const { page } = await params;
+
   if (page === "menswear" || page === "womenswear") {
+    const response = await getPageData(page);
+    console.log(response.data[0].itemPic[0].formats.large);
+
+    // Access the data array from the response
+    const items = response.data || [];
+
     return (
-      <div className="container h-[calc(100vh-8rem)] max-w-screen grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] auto-rows-min gap-4 overflow-auto overflow-x-hidden p-2 sm:p-4">
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
-        <Card />
+      <div className="container h-[calc(100vh-8rem)] max-w-screen grid grid-cols-[repeat(auto-fit,minmax(20rem,1fr))] auto-rows-min gap-4 overflow-auto overflow-x-hidden p-2 pt-0 sm:p-4">
+        {items.map((item) => (
+          <Link key={item.documentId} href={`/${page}/item/${item.documentId}`}>
+            <Card key={item.documentId} props={item} />
+          </Link>
+        ))}
       </div>
     );
   }
 }
 
-// export function generateStaticParams() {
-// return [{ page: "menswear" }, { page: "womenswear" }];
-// }
+export function generateStaticParams() {
+  return [{ page: "menswear" }, { page: "womenswear" }];
+}
