ab4cafc24618b09640e636514af1a964f75b089a
Atakan Temizkan <atikotemizkan@gmail.com>
2025-04-14 14:24:28 +0300

    install sanity

--- src/app/sanity/client.ts
+++
@@ -1,8 +0,0 @@
-import { createClient } from "next-sanity";
-
-export const client = createClient({
-  projectId: "yvajg9ib",
-  dataset: "production",
-  apiVersion: "2025-04-14",
-  useCdn: false,
-});
