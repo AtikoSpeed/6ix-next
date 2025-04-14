74a8d1701b3bc710ebcf1280349f7f74435dcacb
Atakan Temizkan <atikotemizkan@gmail.com>
2025-04-14 14:31:52 +0300

    fix sanity schema import

--- src/sanity/schemaTypes/index.ts
+++ src/sanity/schemaTypes/index.ts
@@ -1,21 +1,24 @@
-import {productType} from './productType'
-import {categoryType} from './categoryType'
-import {collectionType} from './collectionType'
-import {brandType} from './brandType'
-import {sizeType} from './sizeType'
-import {colorType} from './colorType'
-import {designerType} from './designerType'
-import {seasonType} from './seasonType'
-import {genderType} from './genderType'
+import { type SchemaTypeDefinition } from "sanity";
+import { productType } from "./productType";
+import { categoryType } from "./categoryType";
+import { collectionType } from "./collectionType";
+import { brandType } from "./brandType";
+import { sizeType } from "./sizeType";
+import { colorType } from "./colorType";
+import { designerType } from "./designerType";
+import { seasonType } from "./seasonType";
+import { genderType } from "./genderType";
 
-export const schemaTypes = [
-  productType,
-  categoryType,
-  collectionType,
-  brandType,
-  sizeType,
-  colorType,
-  designerType,
-  seasonType,
-  genderType,
-]
+export const schema: { types: SchemaTypeDefinition[] } = {
+  types: [
+    productType,
+    categoryType,
+    collectionType,
+    brandType,
+    sizeType,
+    colorType,
+    designerType,
+    seasonType,
+    genderType,
+  ],
+};
